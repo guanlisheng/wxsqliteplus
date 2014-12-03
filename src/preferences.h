914f3fee1ac160eec9523507f542b92de5cf755b
Lisheng Guan <guanlisheng@gmail.com>
2014-12-03 12:06:56 +0800

    Revert "cleanup inline GPL"
    This reverts commit a6a75ed181a4389924fba10c796b8d2d0a9d0b36.

--- src/preferences.h
+++ src/preferences.h
@@ -1,3 +1,34 @@
+/*---------------------------------------------------------------------------*/
+/* Logiciel de gestion de fichier de base de donnéesSQLite                   */
+/*---------------------------------------------------------------------------*/
+/* Projet  : wxSQLitePlus                              Version  : 0.2.2.0    */
+/* Fichier : preferences.h                                                   */
+/* Auteur  : Fred Cailleau-Lepetit                     Date     : 02/09/2007 */
+/* email   : softinthebox@free.fr                      Révision : 21/08/2008 */
+/*---------------------------------------------------------------------------*/
+/* Copyright (C) Fred Cailleau-Lepetit 2007                                  */
+/* Licence GNU General Public License  http://www.fsf.org/copyleft/gpl.html  */
+/*---------------------------------------------------------------------------*/
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation (version 3).
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Ce programme est libre, vous pouvez le redistribuer et/ou le modifier
+selon les termes de la Licence Publique Générale GNU publiée par la
+Free Software Foundation (version 3).
+
+Ce programme est distribué car potentiellement utile, mais
+SANS AUCUNE GARANTIE, ni explicite ni implicite, y compris
+les garanties de commercialisation ou d'adaptation dans un but
+spécifique. Reportez-vous à la Licence Publique Générale GNU
+pour plus de détails.
+*/
 #ifndef _PREFERENCES_H_
 #define _PREFERENCES_H_
 
--- src/wxsqliteplusapp.cpp
+++ src/wxsqliteplusapp.cpp
@@ -1,3 +1,34 @@
+/*---------------------------------------------------------------------------*/
+/* Logiciel de gestion de fichier de bases de données SQLite                 */
+/*---------------------------------------------------------------------------*/
+/* Projet  : wxSQLitePlus                              Version  : 0.3.0.0    */
+/* Fichier : wxsqliteplusapp.cpp                                             */
+/* Auteur  : Fred Cailleau-Lepetit                     Date     : 04/07/2007 */
+/* email   : softinthebox@free.fr                      Révision : 03/05/2009 */
+/*---------------------------------------------------------------------------*/
+/* Copyright (C) Fred Cailleau-Lepetit 2007                                  */
+/* Licence GNU General Public License  http://www.fsf.org/copyleft/gpl.html  */
+/*---------------------------------------------------------------------------*/
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation (version 3);
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Ce programme est libre, vous pouvez le redistribuer et/ou le modifier
+selon les termes de la Licence Publique Générale GNU publiée par la
+Free Software Foundation (version 3).
+
+Ce programme est distribué car potentiellement utile, mais
+SANS AUCUNE GARANTIE, ni explicite ni implicite, y compris
+les garanties de commercialisation ou d'adaptation dans un but
+spécifique. Reportez-vous à la Licence Publique Générale GNU
+pour plus de détails.
+*/
 
 #if defined(__GNUG__) && !defined(NO_GCC_PRAGMA)
 #pragma implementation "wxsqliteplusapp.h"
